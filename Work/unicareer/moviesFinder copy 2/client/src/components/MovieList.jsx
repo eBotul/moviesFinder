68d923c69eb2778fef0d26ac5d85662bed57c71b
eBotul <emilylurobot@gmail.com>
2024-02-21 17:58:08 -0500

    can fetch movie list from api

--- Work/unicareer/moviesFinder copy 2/client/src/components/MovieList.jsx
+++ Work/unicareer/moviesFinder copy 2/client/src/components/MovieList.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, {useEffect, useContext} from 'react'
+import MoviesFinder from '../apis/MoviesFinder'
+import {MoviesContext} from '../context/MoviesContext'
 
-const MovieList = () => {
+const MovieList = (props) => {
+    const {movies, setMovies} = useContext(MoviesContext)
+    useEffect(()=>{
+      const fetchData = async () => {
+        try {
+          const response = await MoviesFinder.get('/')
+          setMovies(response.data.data)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      fetchData()
+    },[])
+
+
   return (
     <div className='list-group'>
       <table className="table table-hover">
@@ -14,13 +30,24 @@ const MovieList = () => {
             </tr>
         </thead>
         <tbody className="table-success">
-            <tr>
+        {movies && movies.map(movie=>{
+          return (
+            <tr key={movie.id}>  
+              <td>{movie.name}</td>
+              <td>{movie.studio}</td>
+              <td>{movie.ratings}</td>
+              <td><button className="btn btn-warning">Update</button></td>
+              <td><button className="btn btn-danger">Delete</button></td>
+            </tr>
+          )
+        })}
+            {/* <tr>
                 <td>ANT MAN</td>
                 <td>dc</td>
                 <td>3</td>
                 <td><button className="btn btn-warning">Update</button></td>
                 <td><button className="btn btn-danger">Delete</button></td>
-            </tr>
+            </tr> */}
         </tbody>
       </table>
     </div>
